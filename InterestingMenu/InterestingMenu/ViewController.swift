c7439593070c46048458c1a05b80250cf6ad1824
Viraj Padte <virajpadte@gmail.com>
2017-10-25 11:12:14 -0600

    Initial commit

---
+++ InterestingMenu/InterestingMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterestingMenu
+//
+//  Created by Viraj on 10/25/17.
+//  Copyright © 2017 Viraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
