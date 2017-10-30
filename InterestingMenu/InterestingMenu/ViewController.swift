9c1daf3fbb7a0aadb631d741a6e9474f722ec29e
Viraj Padte <virajpadte@gmail.com>
2017-10-30 16:42:27 -0600

    addded a simple circle to the right bottom corner

--- InterestingMenu/InterestingMenu/ViewController.swift
+++ InterestingMenu/InterestingMenu/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let circleSize = CGSize(width: 100.0, height: 100.0);
+        let rectan = UIView(frame: CGRect(origin: CGPoint(x: self.view.frame.width - circleSize.width/2, y: self.view.frame.height - circleSize.height/2), size: circleSize));
+        rectan.layer.cornerRadius = 50.0;
+        rectan.backgroundColor = UIColor.red;
+        self.view.addSubview(rectan);
+        
     }
 
     override func didReceiveMemoryWarning() {
